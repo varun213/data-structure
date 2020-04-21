36a4c883db49100ea61c7b226d54a515556d8786
varun <36834778+varun213@users.noreply.github.com>
2020-04-21 11:49:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var text;
+var news = new XMLHttpRequest();
+
+news.open ('GET', 'data.json' , true);
+news.responseType = 'text';
+news.send(null);
+
+news.onload = function() {
+    if (news.status === 200){
+    text = JSON.parse(news.responseText);
+    console.log(text);
+    display(0);
+    }
+}
